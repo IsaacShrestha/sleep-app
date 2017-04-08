9b64705dbf5210f9c468bdb9715c930da29ac3e4
IsaacShrestha <ishrestha@unomaha.edu>
2017-04-07 22:55:15 -0500

    calculation of bedtime done

--- app/components/alarm-list.js
+++ app/components/alarm-list.js
@@ -37,16 +37,19 @@ export default Ember.Component.extend({
 
 		editWakeup(){
 			this.set('editwakeupAlarm', true);
+			this.set('editbedTimeReminder', false);
 			//this.get('router').transitionTo('editalarms', ['wakeup']);
 
 		},
 
 		editBedtime(){
 			this.set('editbedTimeReminder', true);
+			this.set('editwakeupAlarm', false);
 			//this.get('router').transitionTo('editalarms', ['bedtime']);
 
 		},
 
+		
 		selectHour(value){
 			this.set('hours', value);
 		},
@@ -76,17 +79,56 @@ export default Ember.Component.extend({
 			this.set('alarmTime', alarm ); 
 			this.set('reminder', true);
 			//this.get('router').transitionTo('alarms');
-		}
+		},
 
 		
 	},
 
-	bedtime: Ember.computed(function(){
-		var hours = this.get('hours');
-		var mins = this.get('mins');
-		var totalHours = parseInt(hours)+(parseInt(mins)/60);
-		var bedtimeReminder = parseInt(morningAlarmTime) - 9;
-		return bedtime;
+	bedTime: Ember.computed('hours', 'minutes', 'selectedDayTime', function(){
+		var h = parseInt(this.get('hours'));
+		var m = parseInt(this.get('minutes'));
+		var dayTime = this.get('selectedDayTime');
+		var b = null;
+		var reqdayTime = null;
+		//var t = h + m/60;
+		var reqTime = h - 9;
+		
+		var a = 12 + reqTime;
+		if(a >= 12 && dayTime == "AM"){
+			b = a - 12;
+			reqdayTime = "AM";
+		}
+		else{
+			b = a;
+			reqdayTime = "PM";
+		}
+
+		
+
+		
+		var reqBedTime = b + " : " + m + "  " + reqdayTime;
+		return reqBedTime;
+		
+	}),
+
+	wakeupAlarm: Ember.computed('hours', 'minutes', 'selectedDayTime', function(){
+		var h = parseInt(this.get('hours'));
+		var m = parseInt(this.get('minutes'));
+		var dayTime = this.get('selectedDayTime');
+		var b = null;
+		var reqdayTime = null;
+		//var t = h + m/60;
+		var reqTime = h + 9;
+		
+		var a = reqTime - 12;
+		reqdayTime = "AM";
+		
+		
+
+		
+		var reqWakeTime = a + " : " + m + "  " + reqdayTime;
+		return reqWakeTime;
+
 	})
 
 	
